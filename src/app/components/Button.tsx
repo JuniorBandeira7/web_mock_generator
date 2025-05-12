584b585ab49f3d40fba1285de967dab0b3aa036c
JuniorBandeira7 <juliobandeira77@gmail.com>
2025-05-12 18:04:17 -0300

    Component Button

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,13 @@
+interface ButtonProps {
+        children: any
+        onClick: () => void
+    }
+
+export const Button = (props: ButtonProps) => {
+
+    return (
+        <button onClick={props.onClick} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            {props.children}
+        </button>
+    )
+}
