4640c4b6c45580ab234fae6598757b446426b1b9
JuniorBandeira7 <juliobandeira77@gmail.com>
2025-05-14 11:58:03 -0300

    design GeradorSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import GeradorSection from "./components/GeradorSection";
 
 export default function Home() {
   return (
-    <>
-      <GeradorSection></GeradorSection>
-    </>
+  <main className="mr-auto ml-auto">
+      <div className="mt-16">
+        <GeradorSection></GeradorSection>
+      </div>
+  </main>
   );
 }
